3b80011f85c05a378d161e7f7a7615101818d9bb
dhoouglas <dhoouglas@hotmail.com>
2025-01-27 11:52:41 -0300

    feat: Get click action, add choice button and get user selection

--- App.tsx
+++ App.tsx
@@ -12,8 +12,9 @@ import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
-import { OneSignal } from "react-native-onesignal";
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal";
 import { tagUserInfoCreate } from "./src/notification/notificationsTags";
+import { useEffect } from "react";
 
 OneSignal.initialize("57e42e81-5b5f-434f-93a5-5f922e5df190");
 OneSignal.Notifications.requestPermission(true);
@@ -22,6 +23,34 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();
+
+  useEffect(() => {
+    const handleNotificationCLick = (event: NotificationClickEvent): void => {
+      console.log("Notificação aberta!");
+
+      const { actionId } = event.result;
+
+      switch (actionId) {
+        case "1":
+          console.log("Ver todos");
+          break;
+        case "2":
+          console.log("Ver pedido");
+          break;
+        default:
+          console.log("Nenhum botão de ação selecionado.");
+          break;
+      }
+    };
+
+    OneSignal.Notifications.addEventListener("click", handleNotificationCLick);
+
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        "click",
+        handleNotificationCLick
+      );
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
